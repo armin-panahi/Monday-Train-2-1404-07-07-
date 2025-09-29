61bfd87ef6b49171012989c67da0bb62bf369a3d
𝗔𝗥𝗠𝗜𝗡 𝗣𝗔𝗡𝗔𝗛𝗜 <amirhosseinpanahi.business@gmail.com>
2025-09-29 04:03:56 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Arrow Function's
+
+
+
+// Transform "Named Function" to "Arrow Function"
+let myPlus = (x,y) => x+y;
+
+
+// Transform "Anonymos Function" to "Arrow Function"
+let myPower = (x,y) => {
+    console.log("Result:",x*y);
+};
+
+
+
+
+console.log(myPlus(10,5));
+myPower(4,3);
+
+// Finish
